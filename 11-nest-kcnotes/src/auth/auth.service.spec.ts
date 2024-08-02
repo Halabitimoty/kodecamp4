42fc953076250c10786f50c7ee27828442387923
Halabitimoty <57334680+Halabitimoty@users.noreply.github.com>
2024-08-02 13:21:58 +0100

    Feat: update unit test

--- 11-nest-kcnotes/src/auth/auth.service.spec.ts
+++ 11-nest-kcnotes/src/auth/auth.service.spec.ts
@@ -1,11 +1,11 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AuthService } from './auth.service';
-import { PrismaService } from '../prisma/prisma.service';
-import { JwtService } from '@nestjs/jwt';
-import * as bcrypt from 'bcryptjs';
-import { BadRequestException } from '@nestjs/common';
+import { Test, TestingModule } from "@nestjs/testing";
+import { AuthService } from "./auth.service";
+import { PrismaService } from "../prisma/prisma.service";
+import { JwtService } from "@nestjs/jwt";
+import * as bcrypt from "bcryptjs";
+import { BadRequestException } from "@nestjs/common";
 
-describe('AuthService', () => {
+describe("AuthService", () => {
   let authService: AuthService;
   let prismaService: PrismaService;
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -18,7 +18,7 @@ describe('AuthService', () => {
         {
           provide: JwtService,
           useValue: {
-            sign: jest.fn().mockImplementation(() => 'mockToken'),
+            sign: jest.fn().mockImplementation(() => "mockToken"),
           },
         },
       ],
@@ -30,100 +30,131 @@ describe('AuthService', () => {
   });
 
   const existingUser = {
-    id: '1',
-    username: 'kodecamp',
-    password: 'password',
+    id: "1",
+    username: "kodecamp",
+    password: "password",
     createdAt: new Date(),
     updatedAt: new Date(),
   };
 
-  describe('signup', () => {
-    it('should throw error if user already exists', async () => {
+  describe("signup", () => {
+    it("should throw error if user already exists", async () => {
       jest
-        .spyOn(prismaService.user, 'findUnique')
+        .spyOn(prismaService.user, "findUnique")
         .mockResolvedValue(existingUser);
 
       await expect(authService.signup(existingUser)).rejects.toThrow(
         new BadRequestException(
-          `User with username '${existingUser.username}' already exists.`,
-        ),
+          `User with username '${existingUser.username}' already exists.`
+        )
       );
     });
 
-    it('should create a new user and return a token', async () => {
+    it("should create a new user and return a token", async () => {
       const newUser = {
-        username: 'kodecamp',
-        password: 'password',
+        username: "kodecamp",
+        password: "password",
       };
 
-      jest.spyOn(prismaService.user, 'findUnique').mockResolvedValue(null);
+      jest.spyOn(prismaService.user, "findUnique").mockResolvedValue(null);
       jest
-        .spyOn(bcrypt, 'hash')
-        .mockImplementation(async () => 'hashedPassword');
-      jest.spyOn(prismaService.user, 'create').mockResolvedValue({
+        .spyOn(bcrypt, "hash")
+        .mockImplementation(async () => "hashedPassword");
+      jest.spyOn(prismaService.user, "create").mockResolvedValue({
         ...newUser,
-        id: '1',
-        password: 'hashedPassword',
+        id: "1",
+        password: "hashedPassword",
         createdAt: new Date(),
         updatedAt: new Date(),
       });
       const result = await authService.signup(newUser);
       expect(result).toEqual({
-        token: 'mockToken',
+        token: "mockToken",
       });
     });
   });
 
-  describe('signin', () => {
-    it('should throw an error if user does not exist', async () => {
+  describe("signin", () => {
+    it("should throw an error if user does not exist", async () => {
       const nonExistingUser = {
-        username: 'wronguser',
-        password: 'wrongpassword',
+        username: "wronguser",
+        password: "wrongpassword",
       };
 
-      jest.spyOn(prismaService.user, 'findUnique').mockResolvedValue(null);
+      jest.spyOn(prismaService.user, "findUnique").mockResolvedValue(null);
       await expect(authService.signin(nonExistingUser)).rejects.toThrow(
-        new BadRequestException(`Invalid credentials.`),
+        new BadRequestException(`Invalid credentials.`)
       );
     });
 
-    it('should throw error if password is incorrect', async () => {
+    it("should throw error if password is incorrect", async () => {
       const mockUser = {
-        username: 'kodecamp',
-        password: 'password',
+        username: "kodecamp",
+        password: "password",
       };
-      const hP = 'wrongPassword';
+      const hP = "wrongPassword";
 
-      jest.spyOn(bcrypt, 'hash').mockImplementation(async () => hP);
+      jest.spyOn(bcrypt, "hash").mockImplementation(async () => hP);
       const user = {
         ...existingUser,
         password: hP,
       };
-      jest.spyOn(prismaService.user, 'findUnique').mockResolvedValue(user);
-      jest.spyOn(bcrypt, 'compare').mockImplementation(async () => false);
+      jest.spyOn(prismaService.user, "findUnique").mockResolvedValue(user);
+      jest.spyOn(bcrypt, "compare").mockImplementation(async () => false);
 
       await expect(authService.signin(mockUser)).rejects.toThrow(
-        new BadRequestException(`Invalid credentials.`),
+        new BadRequestException(`Invalid credentials.`)
       );
     });
 
-    it('should return a user if exists and password is correct', async () => {
-      const hP = 'hashedPassword';
-      jest.spyOn(bcrypt, 'hash').mockImplementation(async () => hP);
+    it("should return a user if exists and password is correct", async () => {
+      const hP = "hashedPassword";
+      jest.spyOn(bcrypt, "hash").mockImplementation(async () => hP);
       const mockUser = {
-        id: '1',
+        id: "1",
         username: existingUser.username,
         password: hP,
         createdAt: new Date(),
         updatedAt: new Date(),
       };
-      jest.spyOn(prismaService.user, 'findUnique').mockResolvedValue(mockUser);
-      jest.spyOn(bcrypt, 'compare').mockImplementation(async () => true);
+      jest.spyOn(prismaService.user, "findUnique").mockResolvedValue(mockUser);
+      jest.spyOn(bcrypt, "compare").mockImplementation(async () => true);
 
       const result = await authService.signin(existingUser);
       expect(result).toEqual({
-        token: 'mockToken',
+        token: "mockToken",
       });
+    });
+  });
+
+  describe("update", () => {
+    it("should update a user", async () => {
+      const user = {
+        id: "1",
+        username: "kodecamp",
+        password: "password",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+
+      jest.spyOn(prismaService.user, "update").mockResolvedValue(user);
+      const result = await authService.update(user, user.id);
+      expect(result).toEqual(user);
+    });
+
+    it("should throw an error if user does not exist", async () => {
+      const user = {
+        id: "1",
+        username: "kodecamp",
+        password: "password",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+
+      jest.spyOn(prismaService.user, "update").mockResolvedValue(null);
+      await expect(authService.update(user, user.id)).rejects.toThrow(
+        new BadRequestException(`User not found.`)
+      );
     });
   });
 });
